ab233f7cc9ee8861e2173ba41191ceaf4c2974e1
Perry Cate <NPCate01@gmail.com>
2019-12-22 01:22:21 -0500

    Initial Commit

---
+++ mix.exs
@@ -0,0 +1,25 @@
+defmodule Tldr.MixFile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :tldr,
+      version: "0.1.0",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      applications: [:serum]
+    ]
+  end
+
+  defp deps do
+    [
+      {:serum, "~> 1.3"}
+    ]
+  end
+end
